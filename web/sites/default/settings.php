f4c4154907501f6d64316c6aa7ce7f7d460f81f8
tyler36 <54605-tyler36@users.noreply.drupalcode.org>
2023-11-28 03:45:36 +0000

    add check for pantheon settings file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,32 +1,37 @@
 <?php
 
 /**
+ * @file
  * Load services definition file.
  */
+
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
  * Include the Pantheon-specific settings file.
  *
- * n.b. The settings.pantheon.php file makes some changes
+ * N.b. The settings.pantheon.php file makes some changes
  *      that affect all environments that this site
  *      exists in.  Always include this file, even in
  *      a local development environment, to ensure that
  *      the site settings remain consistent.
  */
-include __DIR__ . "/settings.pantheon.php";
+$pantheon_settings = __DIR__ . "/settings.pantheon.php";
+if (file_exists($pantheon_settings)) {
+  include $pantheon_settings;
+}
 
 /**
  * Skipping permissions hardening will make scaffolding
  * work better, but will also raise a warning when you
  * install Drupal.
  *
- * https://www.drupal.org/project/drupal/issues/3091285
+ * Https://www.drupal.org/project/drupal/issues/3091285
  */
 // $settings['skip_permissions_hardening'] = TRUE;
 
 /**
- * If there is a local settings file, then include it
+ * If there is a local settings file, then include it.
  */
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
@@ -38,7 +43,7 @@ $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
 if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
   require $ddev_settings;
 }
-$databases['default']['default'] = array (
+$databases['default']['default'] = [
   'database' => 'db',
   'username' => 'db',
   'password' => 'db',
@@ -49,5 +54,5 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
   'driver' => 'mysql',
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
+];
 $settings['hash_salt'] = 'A5hCzUAhy4niSZ4Z9VbD09omuntGd67EsU1OBJFndQDSlVMwaZ78nnyb2BhemsHkHHVbJssFMQ';
