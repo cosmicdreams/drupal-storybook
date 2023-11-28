00bcfc3ac9ef02360d955d870d0a67b3cc1ef5d0
Chris.Weber <chris.weber@velir.com>
2023-11-27 21:18:12 -0600

    Storybook-7: Attempt to provide support for storybook 7.5 + Yarn 4.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,22 @@
 module.exports = {
   // Change the place where storybook searched for stories.
   stories: [
-    "../web/**/*.stories.@(mdx|json|yml)",
+    "../web/**/*.stories.@(mdx|yml)",
   ],
+
   // ...
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@lullabot/storybook-drupal-addon', // <----
   ],
-  framework: '@storybook/server',
-  core: {
-    builder: '@storybook/builder-webpack5'
+
+  framework: {
+    name: '@storybook/server-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
   }
 };
